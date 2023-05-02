23f9eb330ade5a7af0dcb893482d85d3e880b6d7
Wenwen Zhou <Wenwen.Zhou@sas.com>
2023-05-02 14:26:11 -0400

    Completed module

--- src/database.py
+++ src/database.py
@@ -1,23 +1,27 @@
 import csv
-from src.deadlined_reminders import DateReminder
-
+from src.deadlined_reminders import DeadlinedReminder
 
 def list_reminders():
     f = open("reminders.csv", "r")
-
     with f:
         reader = csv.reader(f)
-
         for row in reader:
             print()
             for e in row:
                 print(e.ljust(32), end=' ')
         print()
 
-def add_reminder(text, date):
+def add_reminder(reminder):
+    print()
+    reminder = input("What would you like to be reminded about?: ")
 
-    PoliteReminder = {}
+def add_reminder(text, date, ReminderClass):
+    reminder = ReminderClass(text, date)
+
+    if not isinstance(reminder, DeadlinedReminder):
+        raise TypeError('Invalid Reminder Class')
+
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder])
